60afe895282cbec682b524e9b96518d5ad6e0135
Kikii <harshitasadadekar@gmail.com>
2022-02-15 01:47:12 +0530

    created few pages

--- app.js
+++ app.js
@@ -12,9 +12,45 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+let posts = [];
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    newPost: posts
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", {
+    startingContent: aboutContent
+  });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", {
+    startingContent: contactContent
+  });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  
+  res.redirect("/");
+});
 
 
 
@@ -25,9 +61,6 @@ app.use(express.static("public"));
 
 
 
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
